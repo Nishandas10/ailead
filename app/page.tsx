71d2ae95facf64666fedd34183be292aa2d73e1b
Nishant Das <dasbudhe@gmail.com>
2024-02-04 23:39:32 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <Button>Click Me</Button>
+      <UserButton/>
     </div>
   );
 }
-//11:40
+//21:24
